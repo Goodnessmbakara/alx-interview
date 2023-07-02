65669168869a2d3dfe4a0ffe7aa4a2ef84c4261f
Goodnessmbakara <amicablembakara50@gmail.com>
2023-07-02 00:05:24 +0000

    always read me

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 def pascal_triangle(n):
-  """
-  Prints an array of Pascal's triangle.
+    """
+    Prints an array of Pascal's triangle.
 
-  Args:
-    n: The number of rows in the triangle.
-  """
+    Args:
+      n: The number of rows in the triangle.
+    """
+    if n <= 0:
+        return []
 
-  triangle = [[1]]
-  for i in range(1, n):
-    row = [1]
-    for j in range(1, i + 1):
-      row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-    triangle.append(row)
-  for row in triangle:
-    print(row)
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
 
-
-print(pascal_triangle(5))
